345eeb51ffe2037d7cd04be7c9a63c7055eb4c5a
ariefdrm <ariefdarmawan5522@gmail.com>
2024-06-11 11:22:57 +0700

    balajar liner & binary search

---
+++ Binary-search/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi variabel
+            int[] Data = { 3, 9, 11, 12, 15, 17, 23, 31, 35 };
+            int l, r, m, ktm;
+            int n = Data.Length;
+            int cari;
+            l = 0;
+            r = n - 1;
+            ktm = 0;
+
+            // list data
+            foreach (int i in Data)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine( );
+
+            // input data yang dicari
+            Console.Write("masukkan data : ");
+            cari = int.Parse(Console.ReadLine());
+
+            // 
+            while (l <= r && ktm == 0) { 
+                m = (l + r) / 2;
+                Console.WriteLine("data tengah :{0}", m);
+                if (Data[m] == cari)
+                {
+                    ktm = 1;
+                } else if (cari <= Data[m])
+                {
+                    Console.WriteLine("cari di kiri");
+                    r = m - 1;
+                } else
+                {
+                    l = m + 1;
+                    Console.WriteLine("cari dikanan");
+                }
+            }
+
+            if (ktm == 1)
+                Console.WriteLine("data ada!!!");
+            else
+                Console.WriteLine("data tidah ada!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Seq-search/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seq_search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi variabel
+            int[] Data = new int[10];
+            int[] Indeks = new int[10];
+            int j, k;
+
+            // input data
+            for (int i = 0; i < Data.Length; i++)
+            {
+                Console.Write("Data ke {0} : ", i + 1);
+                Data[i] = int.Parse(Console.ReadLine());
+            }
+
+            // memasukkan data yang akan dicari
+            Console.Write("Masukkan data yang dicari : ");
+            k = int.Parse(Console.ReadLine());
+
+            // mencari data
+            j = 0;
+            for (int i = 0; i < Indeks.Length; i++)
+            {
+                if (Data[i] == k)
+                {
+                    Indeks[j] = i;
+                    j++;
+                    break;
+                }
+            }
+
+            // jika data tidak ditemukan didalam array
+            if (j > 0)
+            {
+                Console.Write("Data tersebut tidak ada dalam indeks ke : ");
+                for (int i = 0; i < j; i++)
+                {
+                    Console.WriteLine("{0,3}", Indeks[i]);
+                }
+                Console.WriteLine();
+            }
+
+            // jika tidak ditemukan
+            else
+            {
+                Console.WriteLine("Data tidak ada didalam array!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
